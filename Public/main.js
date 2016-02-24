bc97638f6570e827bca7a4c39679fb8e1a0ba255
Tom Lovett <tomlovett6@gmail.com>
2016-02-24 14:54:03 -0700

    initial commit

---
+++ Public/main.js
@@ -0,0 +1,10 @@
+angular.module('Countries', [])
+
+angular.module('Countries')
+	.controller('Traveller', ['$scope', '$http', function($scope, $http) {
+
+		$scope.loadCountries = function() {
+
+		}
+
+}])
